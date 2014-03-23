431fda82a48839b0bc63c68bbcbfcdba4c4cf3ea
Thiago H. Cândido <thiiagohenriqui@gmail.com>
2014-03-23 13:09:56 -0300

    Projeto pronto para da prosseguimento!

---
+++ ChecklistViagem/src/br/com/aeso/projeto/MainActivity.java
@@ -0,0 +1,37 @@
+package br.com.aeso.projeto;
+
+import android.app.TabActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.widget.TabHost;
+
+public class MainActivity extends TabActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+				
+		TabHost tabHost = getTabHost();
+		
+		LayoutInflater.from(this).inflate(R.layout.abas,
+				tabHost.getTabContentView(), true);
+		
+		tabHost.addTab(tabHost.newTabSpec("tab1")
+				.setIndicator("Aba 1").setContent(R.id.tab1));
+		tabHost.addTab(tabHost.newTabSpec("tab2")
+				.setIndicator("Aba 2").setContent(R.id.tab2));
+		tabHost.addTab(tabHost.newTabSpec("tab3")
+				.setIndicator("Aba 3").setContent(R.id.tab3));
+		
+		setContentView(tabHost);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
