510ef1df5d2fba597bb26a1eb23937e08e5070b2
Thiago H. Cândido <thiiagohenriqui@gmail.com>
2014-04-04 16:12:58 -0300

    Primeira tela feita.....

--- MedicineAlert/gen/br/com/aeso/medicinealert/R.java
+++ MedicineAlert/gen/br/com/aeso/medicinealert/R.java
@@ -25,9 +25,9 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080003;
-        public static final int button1=0x7f080001;
+        public static final int button1=0x7f080000;
         public static final int button2=0x7f080002;
-        public static final int textView1=0x7f080000;
+        public static final int textView1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_cadastra_remedio=0x7f030000;
