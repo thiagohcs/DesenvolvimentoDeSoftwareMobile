2dbd367065a68b22b03e667c034727ac7880bf5e
Thiago H. Cândido <thiiagohenriqui@gmail.com>
2014-04-03 21:35:27 -0300

    Tela inicial quase pronta.....

--- MedicineAlert/gen/br/com/aeso/medicinealert/R.java
+++ MedicineAlert/gen/br/com/aeso/medicinealert/R.java
@@ -24,18 +24,27 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int button1=0x7f080001;
+        public static final int button2=0x7f080002;
+        public static final int textView1=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_home=0x7f030000;
+        public static final int activity_cadastra_remedio=0x7f030000;
+        public static final int activity_home=0x7f030001;
     }
     public static final class menu {
-        public static final int home=0x7f070000;
+        public static final int cadastra_remedio=0x7f070000;
+        public static final int home=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int addRemedio=0x7f050003;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int bemVindo=0x7f050002;
+        public static final int hello_world=0x7f050006;
+        public static final int searchRemedio=0x7f050004;
+        public static final int title_activity_cadastra_remedio=0x7f050005;
     }
     public static final class style {
         /** 
