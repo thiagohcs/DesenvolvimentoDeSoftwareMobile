0fd68dd1ebddbfde64be80efa3dfb3734ebbe3fd
lucascapistrano <capistrano.lucas@hotmail.com>
2014-04-04 19:52:47 -0300

    String.xml alterado.

--- MedicineAlert/gen/br/com/aeso/medicinealert/R.java
+++ MedicineAlert/gen/br/com/aeso/medicinealert/R.java
@@ -48,10 +48,16 @@ public final class R {
         public static final int addRemedio=0x7f050003;
         public static final int app_name=0x7f050000;
         public static final int bemVindo=0x7f050002;
+        public static final int dosagemRemedio=0x7f05000a;
+        public static final int dtInicio=0x7f05000d;
         public static final int hello_world=0x7f050006;
+        public static final int nomeRemedio=0x7f050009;
+        public static final int qtDias=0x7f05000b;
+        public static final int qtVezesDias=0x7f05000c;
         public static final int searchRemedio=0x7f050004;
         public static final int title_activity_cadastra_remedio=0x7f050005;
         public static final int title_activity_consulta_horarios_remedios=0x7f050007;
+        public static final int usuario=0x7f050008;
     }
     public static final class style {
         /** 
