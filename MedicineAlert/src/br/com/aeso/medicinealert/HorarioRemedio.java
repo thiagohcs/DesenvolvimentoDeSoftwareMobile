6e602b51b12852335fc8d0e37999881815d5d487
Thiago H. Cândido <thiiagohenriqui@gmail.com>
2014-04-04 16:55:28 -0300

    Classe PrescriçãoRemedio e HoraRemedio prontas, faltando apenas o conteúdo do método.

---
+++ MedicineAlert/src/br/com/aeso/medicinealert/HorarioRemedio.java
@@ -0,0 +1,34 @@
+package br.com.aeso.medicinealert;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class HorarioRemedio {
+
+	Date data;
+	Time hora;
+
+	public HorarioRemedio() {
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Time getHora() {
+		return hora;
+	}
+
+	public void setHora(Time hora) {
+		this.hora = hora;
+	}
+
+	public void cadastarHorario(Date data, Time hora) {
+
+	}
+
+}
---
+++ MedicineAlert/src/br/com/aeso/medicinealert/PrescricaoRemedio.java
@@ -0,0 +1,82 @@
+package br.com.aeso.medicinealert;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
+
+public class PrescricaoRemedio {
+	
+	String usuario;
+	Date dtInicio;
+	int qtdDias;
+	int qtdVezesDia;
+	float dosagem;
+	String nomeRemedio;
+	List listaHorarioRemedio;
+	
+	public PrescricaoRemedio(){}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public Date getDtInicio() {
+		return dtInicio;
+	}
+
+	public void setDtInicio(Date dtInicio) {
+		this.dtInicio = dtInicio;
+	}
+
+	public int getQtdDias() {
+		return qtdDias;
+	}
+
+	public void setQtdDias(int qtdDias) {
+		this.qtdDias = qtdDias;
+	}
+
+	public int getQtdVezesDia() {
+		return qtdVezesDia;
+	}
+
+	public void setQtdVezesDia(int qtdVezesDia) {
+		this.qtdVezesDia = qtdVezesDia;
+	}
+
+	public float getDosagem() {
+		return dosagem;
+	}
+
+	public void setDosagem(float dosagem) {
+		this.dosagem = dosagem;
+	}
+
+	public String getNomeRemedio() {
+		return nomeRemedio;
+	}
+
+	public void setNomeRemedio(String nomeRemedio) {
+		this.nomeRemedio = nomeRemedio;
+	}
+
+	public List getListaHorarioRemedio() {
+		return listaHorarioRemedio;
+	}
+
+	public void setListaHorarioRemedio(List listaHorarioRemedio) {
+		this.listaHorarioRemedio = listaHorarioRemedio;
+	}
+	
+	public List<Time> sugerirHorario(){
+		return listaHorarioRemedio;
+	}
+	
+	
+	
+	
+}
