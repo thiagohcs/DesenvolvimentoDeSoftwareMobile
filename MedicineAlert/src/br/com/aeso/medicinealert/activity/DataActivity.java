4ebb67775af8dfe84d93de06fca841ebe9110620
Thiago H. Cândido <thiiagohenriqui@gmail.com>
2014-05-25 16:14:42 -0300

    algumas alterações

--- MedicineAlert/src/br/com/aeso/medicinealert/activity/DataActivity.java
+++ MedicineAlert/src/br/com/aeso/medicinealert/activity/DataActivity.java
@@ -3,14 +3,22 @@ package br.com.aeso.medicinealert.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.DatePicker;
 import br.com.aeso.medicinealert.R;
 
 public class DataActivity extends Activity {
-
+	
+	DatePicker data;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_data);
+		
+		data = (DatePicker) findViewById(R.id.datePicker1);
+		
+		//data.set  
 	}
 
 	@Override
